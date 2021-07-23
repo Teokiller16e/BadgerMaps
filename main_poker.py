9514f29f3198b267bbd831a4d22ba3f4fef6b42c
Theodoros Giannilias <32840612+Teokiller16e@users.noreply.github.com>
2021-07-23 15:37:26 +0300

    Define playing and discarded cards

--- main_poker.py
+++ main_poker.py
@@ -18,7 +18,8 @@ def filldeck(symbols):
         else:
             deck[j][0] = (j+1) - 39
             deck[j][1] = symbols[3]
-    number = len(deck)
+
+    # return numpy array with cards and their symbols         
     return deck 
 
 def splitDeck (deck,numPlayers):
@@ -37,8 +38,41 @@ def splitDeck (deck,numPlayers):
         secondCards[i] = deck[secondCard]
         deck[secondCard] = 99
 
+    # return numpy array's with players first and second cards        
     return firstCards,secondCards
     
+
+def burnCards(deck):
+    playingCards = np.empty(shape=[5,2])
+    for i in range(3):
+        discard(playingCards,deck,i)
+    print(deck) 
+    return playingCards
+
+def discard(playingCards,deck,flag):
+    if flag==0:
+        num = 3
+    else: 
+        num = 1
+
+    for j in range(num):
+        indx = randint(0, (len(deck)-1))
+        while(deck[indx][0]==99):
+            indx = randint(0, (len(deck)-1))
+        if(flag==0):
+            playingCards[j] = deck[indx]
+            deck[indx] = 99
+        elif(flag ==1) :
+            playingCards[3] = deck[indx]
+            deck[indx] = 99
+        else:
+            playingCards[4] = deck[indx]
+            deck[indx] = 99
+
+    indx = randint(0, (len(deck)-1))
+    while(deck[indx][0]==99):
+        indx = randint(0, (len(deck)-1))
+    deck[indx] = 99
 
 if __name__ == "__main__":
     symbols =  [0,1,2,3] # ['clubs','diamonds','hearts','spades']
@@ -46,7 +80,8 @@ if __name__ == "__main__":
     numOfPlayers = int(input("Welcome to Badger Map's poker, please give the number of players \n")) # define the number of players 
     while numOfPlayers<2  and numOfPlayers>7:
         numOfPlayers = int(input(" The number you specified is not between the authorized limits, please give number of players between 2 and 7 \n"))
-    splitDeck(deckOfCards,numOfPlayers)
+    firstCards,secondCards = splitDeck(deckOfCards,numOfPlayers) # retrieve players cards
+    playingCards = burnCards(deckOfCards) # retrieve shown up cards 
 
    
 
